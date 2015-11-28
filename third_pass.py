fedd627a294380dbc306b0dacb75cad59153e506
Sam Curley <CurleySamuel@gmail.com>
2015-11-27 22:41:38 -0600

    pipelined remarks model

--- third_pass.py
+++ third_pass.py
@@ -3,158 +3,27 @@ import numpy as np
 import sys
 import matplotlib.pyplot as plt
 from sklearn.cross_validation import cross_val_score
-from time import time
-from operator import itemgetter
-from scipy.stats import randint as sp_randint
+from sklearn.pipeline import Pipeline, make_pipeline
 
 
 def main():
     data = get_data()
     data = normalize_data(data)
-    data, data_original = scalar_data(data)
+    data = binarize_categorical_data(data)
     data_train_x, data_test_x, data_train_y, data_test_y = split_data(data)
+    data_train_x_remarks, data_test_x_remarks, data_train_x, data_test_x = peel_off_remarks(
+        data_train_x, data_test_x)
     print "\nSize of training set: {}\nSize of testing set: {}\nNumber of features: {}\n".format(len(data_train_y), len(data_test_y), len(data_test_x.columns.values))
-    """
-    simple_random_forest(
-        data_train_x, data_test_x, data_train_y, data_test_y)
-    simple_extremely_random_trees(
-        data_train_x, data_test_x, data_train_y, data_test_y)
-    simple_gradient_boosting(
-        data_train_x, data_test_x, data_train_y, data_test_y)
-    fine_tune_gradient_boosting_hyper_params(
-        data_train_x, data_test_x, data_train_y, data_test_y)
-    """
-    text_extraction_and_analysis(
-        data_train_x, data_test_x, data_train_y, data_test_y, data_original)
-
-
-def text_extraction_and_analysis(data_train_x, data_test_x, data_train_y, data_test_y, data):
-    from sklearn.feature_extraction.text import TfidfVectorizer
-    remarks = data['REMARKS'].fillna('')
-    tfidf = TfidfVectorizer(
-        ngram_range=(1, 3),
-        max_df=0.5,
-        min_df=10,
-    )
-    text_features = tfidf.fit_transform(remarks.values)
-
-
-def simple_random_forest(data_train_x, data_test_x, data_train_y, data_test_y):
-    from sklearn.ensemble import RandomForestRegressor
-    print "-- {} --".format("Random Forest Regression using all but remarks")
-    rf = RandomForestRegressor(
-        n_estimators=300,
-        n_jobs=-1
-    )
-    rf.fit(data_train_x, data_train_y)
-    sample_predictions(rf.predict(data_test_x), data_test_y)
-    score = rf.score(data_test_x, data_test_y)
-    cross_validated_scores = cross_val_score(
-        rf, data_test_x, data_test_y, cv=5)
-    print "MSE Accuracy: {}".format(score)
-    print "MSE Across 5 Folds: {}".format(cross_validated_scores)
-    print "95%% Confidence Interval: %0.3f (+/- %0.3f)\n" % (cross_validated_scores.mean(), cross_validated_scores.std() * 1.96)
-
-
-def simple_extremely_random_trees(data_train_x, data_test_x, data_train_y, data_test_y):
-    from sklearn.ensemble import ExtraTreesRegressor
-    print "-- {} --".format("Extremely Randomized Trees Regression using all but remarks")
-    rf = ExtraTreesRegressor(
-        n_estimators=300,
-        n_jobs=-1
-    )
-    rf.fit(data_train_x, data_train_y)
-    sample_predictions(rf.predict(data_test_x), data_test_y)
-    score = rf.score(data_test_x, data_test_y)
-    cross_validated_scores = cross_val_score(
-        rf, data_test_x, data_test_y, cv=5)
-    print "MSE Accuracy: {}".format(score)
-    print "MSE Across 5 Folds: {}".format(cross_validated_scores)
-    print "95%% Confidence Interval: %0.3f (+/- %0.3f)\n" % (cross_validated_scores.mean(), cross_validated_scores.std() * 1.96)
-
-
-def simple_gradient_boosting(data_train_x, data_test_x, data_train_y, data_test_y):
-    from sklearn.ensemble import GradientBoostingRegressor
-    print "-- {} --".format("Gradient Boosting Regression using all but remarks")
-    rf = GradientBoostingRegressor(
-        n_estimators=500,
-        subsample=0.8,
-        learning_rate=0.09,
-        min_samples_leaf=7,
-        min_samples_split=9,
-        max_features=10,
-        max_depth=8
-    )
-    rf.fit(data_train_x, data_train_y)
-    sample_predictions(rf.predict(data_test_x), data_test_y)
-    score = rf.score(data_test_x, data_test_y)
-    cross_validated_scores = cross_val_score(
-        rf, data_test_x, data_test_y, cv=5)
-    print "MSE Accuracy: {}".format(score)
-    print "MSE Across 5 Folds: {}".format(cross_validated_scores)
-    print "95%% Confidence Interval: %0.3f (+/- %0.3f)\n" % (cross_validated_scores.mean(), cross_validated_scores.std() * 1.96)
-
-    """
-    test_score = np.zeros((1000,), dtype=np.float64)
-
-    for i, y_pred in enumerate(rf.staged_predict(data_test_x)):
-        test_score[i] = rf.loss_(data_test_y, y_pred)
-
-    plt.figure(figsize=(12, 6))
-    plt.subplot(1, 2, 1)
-    plt.title('Deviance')
-    plt.plot(np.arange(1000) + 1, rf.train_score_, 'b-',
-             label='Training Set Deviance')
-    plt.plot(np.arange(1000) + 1, test_score, 'r-',
-             label='Test Set Deviance')
-    plt.legend(loc='upper right')
-    plt.xlabel('Boosting Iterations')
-    plt.ylabel('Deviance')
-    plt.show()
-    """
-
-
-def report(grid_scores, n_top=3):
-    top_scores = sorted(grid_scores, key=itemgetter(1), reverse=True)[:n_top]
-    for i, score in enumerate(top_scores):
-        print("Model with rank: {0}".format(i + 1))
-        print("Mean validation score: {0:.3f} (std: {1:.3f})".format(
-              score.mean_validation_score,
-              np.std(score.cv_validation_scores)))
-        print("Parameters: {0}".format(score.parameters))
-        print("")
-
-
-def fine_tune_gradient_boosting_hyper_params(data_train_x, data_test_x, data_train_y, data_test_y):
-    from sklearn.ensemble import GradientBoostingRegressor
-    from sklearn.grid_search import RandomizedSearchCV
-    print "-- {} --".format("Fine-tuning Gradient Boosting Regression")
-    rf = GradientBoostingRegressor(
-        n_estimators=1000
-    )
-    param_dist = {
-        "learning_rate": [0.01, 0.03, 0.05, 0.07, 0.09, 0.1, 0.15, 0.2],
-        "max_depth": sp_randint(1, 15),
-        "min_samples_split": sp_randint(1, 15),
-        "min_samples_leaf": sp_randint(1, 15),
-        "subsample": [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
-        "max_features": sp_randint(1, 15)
-    }
-    n_iter_search = 300
-    random_search = RandomizedSearchCV(
-        rf,
-        param_distributions=param_dist,
-        n_iter=n_iter_search,
-        n_jobs=-1,
-        cv=5,
-        verbose=1
-    )
-
-    start = time()
-    random_search.fit(data_train_x, data_train_y)
-    print("RandomizedSearchCV took %.2f seconds for %d candidates"
-          " parameter settings." % ((time() - start), n_iter_search))
-    report(random_search.grid_scores_)
+    best_model = fit_best_model(data_train_x, data_train_y)
+    report_accuracy(best_model, data_test_x, data_test_y, name="best model")
+    import ipdb
+    ipdb.set_trace()
+    data_train_y_error = data_train_y - best_model.predict(data_train_x)
+    data_test_y_error = data_test_y - best_model.predict(data_test_x)
+    remarks_model = build_remarks_model(
+        data_train_x_remarks, data_train_y_error)
+    report_accuracy(
+        remarks_model, data_test_x_remarks, data_test_y_error, name="remarks model")
 
 
 def get_data():
@@ -189,9 +58,7 @@ def normalize_data(data):
     return data
 
 
-def scalar_data(data):
-    old_data = data.copy()
-
+def binarize_categorical_data(data):
     # Column 'OTHERFEATURES' contains a semicolon seperated
     # string of feature:value pairs. We need to parse those for
     # every row and seperate them into their own columns.
@@ -225,7 +92,7 @@ def scalar_data(data):
             right_index=True
         )
         data = data.drop(var, 1)
-    return data._get_numeric_data(), old_data
+    return data
 
 
 def split_data(data):
@@ -235,14 +102,65 @@ def split_data(data):
     return train_test_split(x, y, test_size=0.25)
 
 
+def peel_off_remarks(data_train_x, data_test_x):
+    return data_train_x['REMARKS'], data_test_x['REMARKS'], data_train_x.drop('REMARKS', 1), data_test_x.drop('REMARKS', 1)
+
+
+def fit_best_model(data_train_x, data_train_y):
+    from sklearn.ensemble import GradientBoostingRegressor
+    rf = GradientBoostingRegressor(
+        n_estimators=500,
+        subsample=0.8,
+        learning_rate=0.09,
+        min_samples_leaf=7,
+        min_samples_split=9,
+        max_features=10,
+        max_depth=8
+    )
+    rf.fit(data_train_x, data_train_y)
+    return rf
+
+
+def report_accuracy(model, data_test_x, data_test_y, name="model"):
+    score = model.score(data_test_x, data_test_y)
+    cross_validated_scores = cross_val_score(
+        model, data_test_x, data_test_y, cv=5)
+    print "{:-^60}".format(name.upper() + " ACCURACY")
+    print "MSE Accuracy: {}".format(score)
+    print "MSE Across 5 Folds: {}".format(cross_validated_scores)
+    print "95%% Confidence Interval: %0.3f (+/- %0.3f)\n" % (cross_validated_scores.mean(), cross_validated_scores.std() * 1.96)
+    sample_predictions(model.predict(data_test_x), data_test_y)
+
+
 def sample_predictions(predicted, actual):
     sample_size = 20
     samples = np.random.randint(0, high=len(actual), size=sample_size)
     print '{:^30}'.format("Predicted"),
-    print '{:^30}\n'.format("Sale Price")
+    print '{:^30}\n'.format("Actual")
     for sample in samples:
         print '{:^30,}'.format(int(predicted[sample])),
         print '{:^30,}'.format(int(actual.iloc[[sample]].values[0]))
+
+
+def build_remarks_model(data_train_x_remarks, data_train_y):
+    from sklearn.feature_extraction.text import TfidfVectorizer
+    from sklearn.decomposition import TruncatedSVD
+    from sklearn.linear_model import LogisticRegression
+    pipe = make_pipeline(
+        TfidfVectorizer(
+            ngram_range=(2, 5),
+            max_df=0.8,
+            min_df=10
+        ),
+        TruncatedSVD(
+            n_components=100
+        ),
+        LogisticRegression(
+        )
+    )
+    pipe.fit(data_train_x_remarks, data_train_y)
+    pipe.set_params(
+        tfidfvectorizer__vocabulary=pipe.named_steps['tfidfvectorizer'].vocabulary_)
 
 
 if __name__ == "__main__":
